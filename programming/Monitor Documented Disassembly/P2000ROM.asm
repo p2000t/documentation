58f5252ef117a3997b7c5454df9fe10f0a0237de
Ferenc Németh <ferencn@gmail.com>
2020-09-24 10:49:49 +0200

    adds assembly instructions

--- programming/Monitor Documented Disassembly/P2000ROM.asm
+++ programming/Monitor Documented Disassembly/P2000ROM.asm
@@ -1,3 +1,8 @@
+; 
+;  please make sure that z80asm P2000ROM.asm && diff a.bin p2000.rom && echo "all good"
+;  prints all good!
+;
+
 include 'Symbols.asm'
 include 'Startup.asm'
 include 'Cassette.asm'
